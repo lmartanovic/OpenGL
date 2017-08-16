bbf6ffa8a7e292ebad53bbfc611b2502aa6297a1
Lukáš Martanovič <lmartanovic@gmail.com>
2017-08-16 20:12:31 +0200

    Enabled ninja build

--- build.sh
+++ build.sh
@@ -1,15 +1,51 @@
 #!/bin/bash
 
-case $1 in
-    "clean")
-        rm -rf build
-        exit 0
-        ;;
-    "*")
-        echo "Nothing to do"
-        exit 0
-        ;;
-esac
+function help {
+    cat <<EOF
+    Usage:
+    -b set buildsystem (MAKE or NINJA), defaults to MAKE
+    -c erase the build directory
+    -h help
+EOF
+}
+
+BUILDSYS=MAKE
+
+while getopts ":b:ch" opt; do
+    case $opt in
+        b)
+            BUILDSYS=$OPTARG
+            ;;
+        c)
+            rm -rf build
+            exit 0
+            ;;
+        h)
+            help
+            exit 0
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            help
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument" >&2
+            help
+            exit 1
+            ;;
+    esac
+done
 
 mkdir -p build
-cd build && cmake .. && make
+
+#TODO use multiple jobs for make
+if [ "$BUILDSYS" == "MAKE" ] ; then
+    cd build && cmake .. && make
+elif [ "$BUILDSYS" == "NINJA" ] ; then
+    cd build && cmake -GNinja .. && ninja -v
+else
+    echo "Invalid buildsystem $BUILDSYS specified - should be one of [MAKE, NINJA]" >&2
+fi
+
+exit 0
